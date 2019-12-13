33d43c4d41c5f1a8ef8eabc377c8e638ab6100e6
Luis Herrera <herrera.lrh@gmail.com>
2019-12-13 22:23:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,6 +64,8 @@ class TicTacToe
         if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] &&
           @board[combo[1]] == @board[combo[2]]
           return combo
+        else
+          false
       end
     end
   end
